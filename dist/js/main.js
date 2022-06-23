6991b01fdca23fba973d0b74acdfda6d9ccf1952
arybkin <arybkin@tmxpub.agiotg.com>
2022-06-23 21:46:53 +0300

    add tabs script; some minor fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,29 @@
+function getTabs(){
+    let tabsTitles = document.querySelectorAll('.tabs__menu li');
+    tabsTitles = [...tabsTitles];
 
-let arr = ['death', 'to', 'all', 'of', 'us'];
-let [el1, el2, el3, ...rest] = arr;
-console.log(rest);
+    let tabsContent = document.querySelectorAll('.tabs__content');
+    tabsContent = [...tabsContent];
+   
+    tabsTitles.forEach((item, index)=>{        
+        item.addEventListener('click', function(){
+            removeClass(tabsTitles, 'active')           
+            this.classList.add('active');
+
+            tabsContent.forEach((item, ind, arr)=>{
+                item.classList.remove('active');
+                if(arr.indexOf(item) == index){
+                   item.classList.add('active')
+                }
+            })
+        })
+    })
+}
+
+function removeClass(arr, className){
+    arr.forEach((item)=>{
+        item.classList.remove(className)
+    })
+}
+
+getTabs();
