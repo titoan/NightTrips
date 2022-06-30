bdd13a3b607efc6a34ee37fe31257e501ed072f6
arybkin <arybkin@tmxpub.agiotg.com>
2022-06-30 11:49:56 +0300

    tablet adaptive for trip sections/tabs

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,60 +1,53 @@
-function getTabs(){
-    let tabsTitles = document.querySelectorAll('.tabs__menu li');
-    tabsTitles = [...tabsTitles];
+function getTabs() {
+  let tabsTitles = document.querySelectorAll(".tabs__menu li");
+  tabsTitles = [...tabsTitles];
 
-    let tabsContent = document.querySelectorAll('.tabs__content');
-    tabsContent = [...tabsContent];
-   
-    tabsTitles.forEach((item, index)=>{        
-        item.addEventListener('click', function(){
-            removeClass(tabsTitles, 'active')           
-            this.classList.add('active');
+  let tabsContent = document.querySelectorAll(".tabs__content");
+  tabsContent = [...tabsContent];
 
-            tabsContent.forEach((item, ind, arr)=>{
-                item.classList.remove('active');
-                if(arr.indexOf(item) == index){
-                   item.classList.add('active')
-                }
-            })
-        })
-    })
+  tabsTitles.forEach((item, index) => {
+    item.addEventListener("click", function () {
+      removeClass(tabsTitles, "active");
+      this.classList.add("active");
 
-    function removeClass(arr, className){
-        arr.forEach((item)=>{
-            item.classList.remove(className)
-        })
-    }
+      tabsContent.forEach((item, ind, arr) => {
+        item.classList.remove("active");
+        if (arr.indexOf(item) == index) {
+          item.classList.add("active");
+        }
+      });
+    });
+  });
+
+  function removeClass(arr, className) {
+    arr.forEach((item) => {
+      item.classList.remove(className);
+    });
+  }
 }
 getTabs();
 
-function getSlider(){
-    let sliderContainer = document.querySelector('.tab-slider__container');
-    let sliderNavElems = document.querySelectorAll('.tab-slider__nav');  
-    sliderNavElems = [...sliderNavElems];
-    
-    sliderNavElems.forEach(function(item){
-        let [navPrev, navNext] = item.children
-        let width = 0;
+function getSlider() {
+  let sliderContainer = document.querySelector(".tab-slider__container");
+  let sliderNavElems = document.querySelectorAll(".tab-slider__nav");
+  sliderNavElems = [...sliderNavElems];
 
-        navPrev.addEventListener('click', function(){
-            let a = this.parentElement.previousSibling;
-            width += 486                 
-            a.style.right = width + 'px'
-            
-        })
+  sliderNavElems.forEach(function (item) {
+    let [navPrev, navNext] = item.children;
+    let width = 0;
 
-        navNext.addEventListener('click', function(){
-            let a = this.parentElement.previousSibling;
-            width -= 486                 
-            a.style.right = width + 'px'
-        })
-    })
-    
+    navPrev.addEventListener("click", function () {
+      let a = this.parentElement.previousSibling;
+      width += 486;
+      a.style.right = width + "px";
+    });
 
-
-    // nextNav.addEventListener('click', function(){
-    //    console.log('dfsfdsf')
-    // })
+    navNext.addEventListener("click", function () {
+      let a = this.parentElement.previousSibling;
+      width -= 486;
+      a.style.right = width + "px";
+    });
+  });
 }
 
-getSlider()
+getSlider();
